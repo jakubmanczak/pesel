cae1f619c1f8c0465def9de19dfae4a54790a13c
jakubmanczak <jakub@manczak.net>
2022-09-24 19:40:13 +0200

    init & project start

---
+++ src/config.ts
@@ -0,0 +1,33 @@
+export const config = {
+	uitext: {
+		maintitle: ["PESEL", "PESEL"],
+		subtitle: [
+			"PESEL number verificator and analyzer.",
+			"Analizator i weryfikator numeru PESEL.",
+		],
+		inputplaceholder: [
+			"Enter a PESEL number to check. (11 digits)",
+			"Podaj numer PESEL do sprawdzenia. (11 cyfr)",
+		],
+		inputstatus: {},
+		results: {
+			preresult: [
+				"Enter a PESEL number to check.",
+				"Podaj numer PESEL do sprawdzenia.",
+			],
+			incorrect: ["Incorrect PESEL number.", "Niepoprawny numer PESEL."],
+			resultstitle: [
+				"Information encoded in the PESEL above",
+				"Informacje zawarte w podanym PESEL-u",
+			],
+			presubresult: [
+				"A suitable PESEL number should consist of 11 digits.",
+				"PESEL powinien składać się z 11 cyfr.",
+			],
+			disclaimer: [
+				"All verification and analysis is performed in your browser - no data is sent anywhere.",
+				"Całość weryfikacji i analizy dzieje się w przeglądarce - dane nie są nigdzie wysyłane.",
+			],
+		},
+	},
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import {config} from '../config';
+	let input: string = "";
+	let lang: number = 1;
+	let status: string = "";
+	let correct: boolean = false;
+</script>
+
+<main>
+	<h1>{config.uitext.maintitle[lang]}</h1>
+	<p>{config.uitext.subtitle[lang]}</p>
+	<input bind:value={input} placeholder={config.uitext.inputplaceholder[lang]}>
+</main>
+
+<article>
+	{#if input.length === 11 && correct}
+		<h2>{config.uitext.results.resultstitle[lang]}</h2>
+	{:else if input.length === 11 && !correct}
+		<h2>{config.uitext.results.incorrect[lang]}</h2>
+	{:else}
+		<h4>{config.uitext.results.preresult[lang]}</h4>
+		<p>{config.uitext.results.presubresult[lang]}</p>
+	{/if}
+	<footer>
+		<p>
+			{config.uitext.results.disclaimer[lang]}
+		</p>
+		<a href="https://manczak.net" class="author">
+			<p>~ Jakub Mańczak, 2022</p>
+			<img src="https://avatars.githubusercontent.com/u/56172798" alt="jakub mańczak in 2022">
+		</a>
+	</footer>
+</article>
